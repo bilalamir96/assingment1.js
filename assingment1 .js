10467104d3c1c3e6cd6694096790ca5fe5f08ec6
bilalamir96 <156606418+bilalamir96@users.noreply.github.com>
2024-01-25 12:42:02 +0500

    Add files via upload

---
+++ assingment1 .js
@@ -0,0 +1,9 @@
+alert ("welcome message to the user when they open the webpage");
+
+let student1 = "ali";
+let age = 19 ;
+let isstudent = true ;
+
+console.log ( student1);
+console.log ( age);
+console.log ( isstudent);
